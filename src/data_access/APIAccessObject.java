213e9102427d3c54ba7b064e8d629f4ab556bf04
mebsim <ebsim.mohamed@gmail.com>
2023-11-09 13:41:43 -0500

    Created a more formalized way of sending prompts. Need to build the surrounding elements.

--- src/data_access/APIAccessObject.java
+++ src/data_access/APIAccessObject.java
@@ -10,20 +10,24 @@ import java.util.List;
 
 public class APIAccessObject {
 
-    // TODO: Fill this in
+    private final OpenAiService service;
 
-    // THIS IS EXAMPLE CODE
     private static final String API_TOKEN = System.getenv("API_TOKEN");
 
     public static String getApiToken() {
         return API_TOKEN;
     }
 
-    public static void runChatGPT() {
-        OpenAiService service = new OpenAiService(getApiToken());
+    public APIAccessObject() {
+        service = new OpenAiService(getApiToken());
+    }
+
+
+
+    public void runChatGPT(String systemPrompt, String userPrompt) {
         final List<ChatMessage> messages = new ArrayList<>();
-        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are an assistant for a dungeon master for a simplified version of Dungeons & Dragons. You need to be concise and accurate, limiting your statements to a short sentence or just numbers.");
-        final ChatMessage requestMessage = new ChatMessage(ChatMessageRole.USER.value(), "Generate the stats for an enemy that a level 1 warrior can fight.");
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), systemPrompt);
+        final ChatMessage requestMessage = new ChatMessage(ChatMessageRole.USER.value(), userPrompt);
         messages.add(systemMessage);
         messages.add(requestMessage);
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
