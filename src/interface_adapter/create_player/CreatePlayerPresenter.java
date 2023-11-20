8276128dcde37eb3f833619c1a546c09a5857410
samildhawan <samildhawan@hotmail.com>
2023-11-20 14:01:55 -0500

    Created CreatePlayer Interface and all relevant classes for user to choose their character for the game

--- src/interface_adapter/create_player/CreatePlayerPresenter.java
+++ src/interface_adapter/create_player/CreatePlayerPresenter.java
@@ -2,6 +2,7 @@ package src.interface_adapter.create_player;
 
 import src.interface_adapter.ViewManagerModel;
 import src.use_case.create_player.CreatePlayerOutputBoundary;
+import src.use_case.create_player.CreatePlayerOutputData;
 
 public class CreatePlayerPresenter implements CreatePlayerOutputBoundary {
     private final ViewManagerModel viewManagerModel;
@@ -11,5 +12,19 @@ public class CreatePlayerPresenter implements CreatePlayerOutputBoundary {
                                  CreatePlayerViewModel createPlayerViewModel){
         this.viewManagerModel = viewManagerModel;
         this.createPlayerViewModel = createPlayerViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(CreatePlayerOutputData data){
+        CreatePlayerState currentState = createPlayerViewModel.getState();
+        currentState.setHistory(currentState.getHistory() + "\n" + data.getMessage());
+        createPlayerViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error){
+        CreatePlayerState currentState = createPlayerViewModel.getState();
+        currentState.setPromptError("Failed to save information. Please try again.");
+        createPlayerViewModel.firePropertyChanged();
     }
 }
