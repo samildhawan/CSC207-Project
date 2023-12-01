761cfb376fa4864dd17783a2d78cf7ad1a68f0f1
maryannchen <maryann.chen@mail.utoronto.ca>
2023-12-01 14:10:47 -0500

    Added javadocs to existing code in the Entities folder and for the ChangeWorld use case.

--- src/entity/Character.java
+++ src/entity/Character.java
@@ -1,3 +1,14 @@
+/**
+ * The Character class represents a character in the game world. It encapsulates information
+ * about the character's name and relevance within the context of the game.
+ *
+ * @author Maryann Chen
+ * @author Samil Dhawan
+ * @author Mohamed Ebsim
+ * @since 17.0.8.1
+ *
+ */
+
 package src.entity;
 
 public class Character {
@@ -5,13 +16,32 @@ public class Character {
     private final String name;
     private final String relevance;
 
+    /**
+     * Constructs a new Character with the specified name and relevance.
+     *
+     * @param name      The name of the character.
+     * @param relevance The relevance of the character within the context of the game.
+     */
+
     Character(String name, String relevance) {
         this.name = name;
         this.relevance = relevance;
     }
+
+    /**
+     * Gets the name of the character.
+     *
+     * @return The name of the character.
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Gets the relevance of the character within the context of the game.
+     *
+     * @return The relevance of the character.
+     */
     public String getRelevance() { return relevance; }
 
 }
