942f8d23464f4625659a52993655f9cc679dceec
mebsim <ebsim.mohamed@gmail.com>
2023-12-03 19:00:54 -0500

    Updated and extended testing for the CreatePlayer use case.

--- test/view/CreatePlayerViewTest.java
+++ test/view/CreatePlayerViewTest.java
@@ -33,7 +33,7 @@ public class CreatePlayerViewTest {
 
         JPanel jp2 = (JPanel) jp.getComponent(0);
 
-        return (CreatePlayerView) jp2.getComponent(0);
+        return (CreatePlayerView) jp2.getComponent(1);
     }
 
     public JButton getButton(){
@@ -54,7 +54,7 @@ public class CreatePlayerViewTest {
 
         JPanel jp2 = (JPanel) jp.getComponent(0);
 
-        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(0);
+        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(1);
 
         JPanel buttons = (JPanel) sv.getComponent(2);
 
@@ -79,7 +79,7 @@ public class CreatePlayerViewTest {
 
         JPanel jp2 = (JPanel) jp.getComponent(0);
 
-        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(0);
+        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(1);
 
         return (JScrollPane) sv.getComponent(0);
     }
@@ -103,7 +103,7 @@ public class CreatePlayerViewTest {
 
         JPanel jp2 = (JPanel) jp.getComponent(0);
 
-        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(0);
+        CreatePlayerView sv = (CreatePlayerView) jp2.getComponent(1);
 
         return (JTextArea) sv.getComponent(1);
     }
@@ -111,11 +111,11 @@ public class CreatePlayerViewTest {
     @org.junit.Test
     public void testCreatePlayer() {
         // Create a player with sample data
-        Player player = new CreatePlayer("John Doe", 0,2, "Warrior", "A brave warrior with a mysterious past.");
+        Player player = new CreatePlayer("John Doe", "Warrior", "A brave warrior with a mysterious past.");
 
         // Check if the player's attributes are set correctly
         assertEquals("John Doe", player.getName());
-        assertEquals(2, player.getXP());
+        assertEquals(0, player.getXP());
         assertEquals("Warrior", player.getClassType());
         assertEquals("A brave warrior with a mysterious past.", player.getBackstory());    // Assuming initial XP is 0
 
@@ -129,7 +129,18 @@ public class CreatePlayerViewTest {
         Main.main(null);
         CreatePlayerView createPlayerView = getCreatePlayerView();
         JButton button = getButton();
-        assert(button.getText().equals("Submit"));
+        assert(button.getText().equals("Confirm your choice"));
+    }
+
+    @org.junit.Test
+    public void testBackstoryGeneration() {
+        Main.main(null);
+        JButton button = getButton();
+        JTextArea history = (JTextArea) getHistoryTextArea().getViewport().getView();
+
+        assert(history.getText().isEmpty());
+        button.doClick();
+        assert(!history.getText().isEmpty());
     }
 
 
